3bac1e728512530aa513da27b6b238a212a40d34
Ken Sodemann <ken.sodemann@gmail.com>
2017-05-27 09:25:39 -0400

    feat(auth): display information about current auth status

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -8,8 +8,22 @@ import { AngularFireAuth } from 'angularfire2/auth';
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  displayName: string;
+  url: string;
 
   constructor(public navCtrl: NavController, private afAuth: AngularFireAuth) { }
+
+  ionViewDidLoad() {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.displayName = user.displayName;
+        this.url = user.photoURL;
+      } else {
+        this.displayName = null;
+        this.url = null;
+      }
+    });
+  }
 
   signOut() {
     this.afAuth.auth.signOut();
